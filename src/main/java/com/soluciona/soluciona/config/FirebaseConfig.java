c10b587a1d55458e2e65e292631cd00f207fc4b9
Yan Maia <yandantasmaia12@gmail.com>
2025-12-17 22:09:59 +0000

    -fix: firebase credentials

--- src/main/java/com/soluciona/soluciona/config/FirebaseConfig.java
+++ src/main/java/com/soluciona/soluciona/config/FirebaseConfig.java
@@ -7,26 +7,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
+    String jsonCredentials = System.getenv("FIREBASE_CREDENTIALS");
 
-    @Bean
-    public FirebaseApp firebaseApp() throws IOException {
+    InputStream serviceAccount = new ByteArrayInputStream(jsonCredentials.getBytes());
 
-        if (FirebaseApp.getApps().isEmpty()) {
+    FirebaseOptions options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build();
 
-            InputStream serviceAccount = new ClassPathResource("firebase-service-account.json").getInputStream();
-
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            return FirebaseApp.initializeApp(options);
-        }
-
-        return FirebaseApp.getInstance();
+    public FirebaseConfig() throws IOException {
     }
 }
